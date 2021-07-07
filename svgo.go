c700317db1439f059b38fe865e171504aa44e2ff
a-poor <45295232+a-poor@users.noreply.github.com>
2021-07-07 10:23:31 -0700

    Updating element => string methods

--- svgo.go
+++ svgo.go
@@ -6,161 +6,153 @@ import (
 )
 
 type SVGElement interface {
-	toString() string
+	ToString() string
 }
 
 type SVGParent interface {
-	addChild(c SVGElement)
+	AddChild(c SVGElement)
 }
-
-//type Element struct {
-//	attributes map[string]string
-//	children []Element
-//}
 
 type Point2D struct {
 	x, y float64
 }
 
-
 type SVG struct {
-	width  int32
-	height int32
-	children []SVGElement
+	width      int32
+	height     int32
+	children   []SVGElement
 	attributes map[string]string
 }
-
-
 
 type Group struct {
 	attributes map[string]string
-	children []SVGElement
+	children   []SVGElement
 }
 
 type Circle struct {
-	cx float64
-	cy float64
-	r  float64
+	cx         float64
+	cy         float64
+	r          float64
 	attributes map[string]string
 }
 
 type Ellipse struct {
-	cx float64
-	cy float64
-	rx float64
-	ry float64
+	cx         float64
+	cy         float64
+	rx         float64
+	ry         float64
 	attributes map[string]string
 }
 
 type Rect struct {
-	x float64
-	y float64
-	width float64
-	height float64
+	x          float64
+	y          float64
+	width      float64
+	height     float64
 	attributes map[string]string
 }
 
 type Line struct {
-	x1 float64
-	y1 float64
-	x2 float64
-	y2 float64
+	x1         float64
+	y1         float64
+	x2         float64
+	y2         float64
 	attributes map[string]string
 }
 
 type Text struct {
-	text string
-	x float64
-	y float64
+	text       string
+	x          float64
+	y          float64
 	attributes map[string]string
 }
 
 type Polygon struct {
 	attributes map[string]string
-	points []Point2D
+	points     []Point2D
 }
 
-
-
-func (svg SVG) toString() string {
+func (svg SVG) ToString() string {
 	sattrs := formatAttributes(svg.attributes)
 	schildren := ""
 	for _, c := range svg.children {
-		schildren += c.toString()
+		schildren += c.ToString()
 	}
 	return fmt.Sprintf("<svg viewbox=\"0 0 %d %d\" xmlns=\"http://www.w3.org/2000/svg\" %s>%s</svg>",
 		svg.width, svg.height, sattrs, schildren)
 }
 
-func (g Group) toString() string {
+func (g Group) ToString() string {
 	sattrs := formatAttributes(g.attributes)
 	schildren := ""
 	for _, c := range g.children {
-		schildren += c.toString()
+		schildren += c.ToString()
 	}
 	return fmt.Sprintf("<g %s>%s</g>", sattrs, schildren)
 }
 
-func (c Circle) toString() string {
+func (c Circle) ToString() string {
 	sattrs := formatAttributes(c.attributes)
 	return fmt.Sprintf("<circle cx=\"%f\" cy=\"%f\" r=\"%f\" %s />", c.cx, c.cy, c.r, sattrs)
 }
 
-func (e Ellipse) toString() string {
+func (e Ellipse) ToString() string {
 	sattrs := formatAttributes(e.attributes)
 	return fmt.Sprintf("<ellipse cx=\"%f\" cy=\"%f\" rx=\"%f\" rx=\"%f\" %s />",
 		e.cx, e.cy, e.rx, e.ry, sattrs)
 }
 
-func (r Rect) toString() string {
+func (r Rect) ToString() string {
 	sattrs := formatAttributes(r.attributes)
 	return fmt.Sprintf("<rect x=\"%f\" y=\"%f\" width=\"%f\" height=\"%f\" %s />",
 		r.x, r.y, r.width, r.height, sattrs)
 }
 
-func (l Line) toString() string {
+func (l Line) ToString() string {
 	sattrs := formatAttributes(l.attributes)
 	return fmt.Sprintf("<line x1=\"%f\" y1=\"%f\" x2=\"%f\" y2=\"%f\" %s />",
 		l.x1, l.y1, l.x2, l.y2, sattrs)
 }
 
-func (t Text) toString() string {
+func (t Text) ToString() string {
 	sattrs := formatAttributes(t.attributes)
-	return fmt.Sprintf("<text x=\"%s\" y=\"%\" %s>%s</text>",
+	return fmt.Sprintf("<text x=\"%f\" y=\"%f\" %s>%s</text>",
 		t.x, t.y, sattrs, t.text)
 }
 
-func (p Polygon) toString() string {
+func (p Polygon) ToString() string {
 	sattrs := formatAttributes(p.attributes)
 	points := make([]string, 0)
-	for _, p := range p.points {
-		points = append(points, fmt.Sprintf("%f,%f", p.x, p.y))
+	for _, pt := range p.points {
+		points = append(points, pt.ToString())
 	}
 	spoints := strings.Join(points, " ")
 	return fmt.Sprintf("<polygon points=\"%s\" %s/>", spoints, sattrs)
 }
 
+func (p Point2D) ToString() string {
+	return fmt.Sprintf("%f,%f", p.x, p.y)
+}
 
-func (svg SVG) addChild(child SVGElement) {
+func (svg SVG) AddChild(child SVGElement) {
 	svg.children = append(svg.children, child)
 }
 
-func (g Group) addChild(child SVGElement) {
+func (g Group) AddChild(child SVGElement) {
 	g.children = append(g.children, child)
 }
 
-
-func (pg Polygon) addPoint2D(p Point2D) {
+func (pg Polygon) AddPoint2D(p Point2D) {
 	pg.points = append(pg.points, p)
 }
 
-func (pg Polygon) addPoint(x, y float64) {
-	pg.addPoint2D(Point2D{x, y})
+func (pg Polygon) AddPoint(x, y float64) {
+	pg.AddPoint2D(Point2D{x, y})
 }
 
 func formatAttributes(attrs map[string]string) string {
 	var sattrs string
-	for k, v := range(attrs) {
+	for k, v := range attrs {
 		sattrs += fmt.Sprintf("%s=\"%s\" ", k, v)
 	}
 	return strings.TrimSpace(sattrs)
