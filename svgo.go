48dd82af4aca2eff4b2a589cbd4725606ea4a60e
a-poor <45295232+a-poor@users.noreply.github.com>
2021-07-07 21:50:59 -0700

    Moving all SVG types to one general `Element` type
    TODO: Write functions to create specific SVG elements using
    specific params

--- svgo.go
+++ svgo.go
@@ -5,23 +5,46 @@ import (
 	"strings"
 )
 
-type SVGElement interface {
-	ToString() string
-	//GetAttr(key string) string
-	//SetAttr(key string, value string) string
-	//DelAttr(key string) string
+type Element struct {
+	TagName string
+	Attributes map[string]string
+	Children []interface{}
 }
 
-type SVGParent interface {
-	AddChild(c SVGElement)
+type Stringer interface {
+	String() string
+}
+
+func (e Element) String() string {
+	return fmt.Sprintf(
+		"<%s %s>%s</%s>",
+		e.TagName,
+		formatAttributes(e.Attributes),
+		formatChildren(e.Children),
+		e.TagName,
+	)
 }
 
 type Point2D struct {
 	X, Y float64
 }
 
-func (p Point2D) ToString() string {
+func (p Point2D) String() string {
 	return fmt.Sprintf("%f,%f", p.X, p.Y)
+}
+
+func formatChildren(children []interface{}) string {
+	res := ""
+	for _, c := range children {
+		e, ok := c.(Element)
+		if ok { // `c` is an `Element`
+			res += e.String()
+		} else { // Treat `c` as a `string`
+			s, _ := c.(string)
+			res += s
+		}
+	}
+	return res
 }
 
 func formatAttributes(attrs map[string]string) string {
